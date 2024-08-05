92070dee71610325176024dbf9f7d4d83fc29149
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-06 01:39:40 +0200

    fetched questions and checked the response

---
+++ src/api.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const fetchQuestions = async (difficulty) => {
+    try {
+        const response = await axios.get(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`)
+        return response.data.results
+    
+    } catch (error) {
+        console.error('Error fetching questions:)', error);
+        throw error;
+    
+ }   
+}
+
+
+
