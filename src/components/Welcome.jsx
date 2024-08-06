1629af84badd5d3781f339a05ccb208689b83991
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-06 02:09:06 +0200

    Update Welcome.jsx

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+// import { QuizContext } from '../QuizContext';
+
+const Welcome = () => {
+    // const { setDifficulty } = useContext(QuizContext);
+  
+    return (
+      <div className="welcome-page">
+        <h1 className='h1'>Welcome to the Quiz</h1>
+        <p className='text'>Select difficulty level:</p>
+        <div>
+          <button className='btn1' onClick={() => setDifficulty('easy')}>Easy</button>
+          <button className='btn2' onClick={() => setDifficulty('medium')}>Medium</button>
+          <button className='btn3' onClick={() => setDifficulty('hard')}>Hard</button>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Welcome;
