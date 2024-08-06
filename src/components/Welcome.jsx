3b26ea512226ba822509af7244a0858561a08a1c
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-06 02:21:32 +0200

    setting css file for welcome page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+
+import './Welcome.css';
 // import { QuizContext } from '../QuizContext';
 
 const Welcome = () => {
@@ -8,7 +9,7 @@ const Welcome = () => {
       <div className="welcome-page">
         <h1 className='h1'>Welcome to the Quiz</h1>
         <p className='text'>Select difficulty level:</p>
-        <div>
+        <div className='buttons'>
           <button className='btn1' onClick={() => setDifficulty('easy')}>Easy</button>
           <button className='btn2' onClick={() => setDifficulty('medium')}>Medium</button>
           <button className='btn3' onClick={() => setDifficulty('hard')}>Hard</button>
