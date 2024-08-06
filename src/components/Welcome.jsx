a8ac6f5ca6c65df6b97a4dfa3b301bcbf769f540
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-06 20:08:36 +0200

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -7,13 +7,16 @@ const Welcome = () => {
   
     return (
       <div className="welcome-page">
+        <section className='container'>
         <h1 className='h1'>Welcome to the Quiz</h1>
         <p className='text'>Select difficulty level to start the quiz:</p>
         <div className='buttons'>
           <button className='btn1' onClick={() => setDifficulty('easy')}>Easy</button>
           <button className='btn2' onClick={() => setDifficulty('medium')}>Medium</button>
           <button className='btn3' onClick={() => setDifficulty('hard')}>Hard</button>
-        </div>
+         
+          </div>
+          </section>
       </div>
     );
   };
