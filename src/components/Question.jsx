f1b1e0d7a22c9ed29a46e8ab1add00056dfe1ce6
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-07 17:56:56 +0200

    Update Question.jsx

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import AnswerButton from './AnswerButton';
 import './Question.css';
 import parse from 'html-react-parser';
 
@@ -14,14 +13,14 @@ const Question = ({ question, handleAnswer, selectedAnswer }) => {
       <p>{parse(question.question)}</p>
       <div className="answer-buttons">
         {answers.map((answer, index) => (
-          <AnswerButton
+          <button
             key={index}
-            answer={answer}
             onClick={() => handleAnswer(answer)}
-            isCorrect={answer === question.correct_answer}
-            isSelected={selectedAnswer === answer}
-            disabled={selectedAnswer !== ''}
-          />
+            className={selectedAnswer ? (answer === question.correct_answer ? 'correct' : 'incorrect') : ''}
+            disabled={selectedAnswer !== '' && selectedAnswer !== answer}
+          >
+            {parse(answer)}
+          </button>
         ))}
       </div>
     </div>
@@ -29,3 +28,67 @@ const Question = ({ question, handleAnswer, selectedAnswer }) => {
 };
 
 export default Question;
+
+// import React from 'react';
+// import AnswerButton from './AnswerButton';
+// import './Question.css';
+// import parse from 'html-react-parser';
+
+// const Question = ({ question, handleAnswer, selectedAnswer }) => {
+//   const isMultipleChoice = question.type === 'multiple';
+//   const answers = isMultipleChoice
+//     ? question.incorrect_answers.concat(question.correct_answer).sort()
+//     : ['True', 'False'];
+
+//   return (
+//     <div>
+//       <p>{parse(question.question)}</p>
+//       <div className="answer-buttons">
+//         {answers.map((answer, index) => (
+//           <AnswerButton
+//             key={index}
+//             answer={answer}
+//             onClick={() => handleAnswer(answer)}
+//             isCorrect={answer === question.correct_answer}
+//             isSelected={selectedAnswer === answer}
+//             disabled={selectedAnswer !== '' && selectedAnswer !== answer}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Question;
+
+// import React from 'react';
+// import AnswerButton from './AnswerButton';
+// import './Question.css';
+// import parse from 'html-react-parser';
+
+// const Question = ({ question, handleAnswer, selectedAnswer }) => {
+//   const isMultipleChoice = question.type === 'multiple';
+//   const answers = isMultipleChoice
+//     ? question.incorrect_answers.concat(question.correct_answer).sort()
+//     : ['True', 'False'];
+
+//   return (
+//     <div>
+//       <p>{parse(question.question)}</p>
+//       <div className="answer-buttons">
+//         {answers.map((answer, index) => (
+//           <AnswerButton
+//             key={index}
+//             answer={answer}
+//             onClick={() => handleAnswer(answer)}
+//             isCorrect={answer === question.correct_answer}
+//             isSelected={selectedAnswer === answer}
+//             disabled={selectedAnswer !== ''}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Question;
