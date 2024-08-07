0923565c2147b6e47cfbb1c20cd438e055172a07
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-07 17:56:41 +0200

    Update AnswerButton.jsx

--- src/components/AnswerButton.jsx
+++ src/components/AnswerButton.jsx
@@ -1,48 +1,43 @@
-
 import React from 'react';
 import parse from 'html-react-parser';
+import './AnswerButton.css';
 
 const AnswerButton = ({ answer, onClick, isCorrect, isSelected, disabled }) => {
   return (
-    <button
-      onClick={onClick}
-      className={isSelected
-        ? isCorrect
-          ? 'correct'
-          : 'incorrect'
-        : ''
-      }
-      disabled={disabled}
-    >
-      {parse(answer)}
-    </button>
+       <button
+        onClick={onClick}
+        className={isSelected ? (isCorrect ? 'correct' : 'incorrect') : ''}
+        disabled={disabled}
+      >
+        {parse(answer)}
+      </button>
+   
   );
 };
 
 export default AnswerButton;
 
+
 // import React from 'react';
-// // import parse from 'html-react-parser';'
-
-
-// import './AnswerButton.css';
+// import parse from 'html-react-parser';
 
 // const AnswerButton = ({ answer, onClick, isCorrect, isSelected, disabled }) => {
 //   return (
 //     <button
 //       onClick={onClick}
+//       className={isSelected
+//         ? isCorrect
+//           ? 'correct'
+//           : 'incorrect'
+//         : ''
+//       }
 //       disabled={disabled}
-//       className="answer-button"
 //     >
-//       {(answer)}
-//       {isSelected && (
-//         <img
-//           src={isCorrect ? correctImg : incorrectImg}
-//           alt={isCorrect ? 'Correct' : 'Incorrect'}
-//         />
-//       )}
+//       {parse(answer)}
 //     </button>
 //   );
 // };
 
 // export default AnswerButton;
+
+
